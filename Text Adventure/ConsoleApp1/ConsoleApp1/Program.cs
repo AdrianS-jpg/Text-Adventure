b9950dd330e533c35f5fe323a28b5acfeca36cb2
MUKILTEO\1721032 <1721032@mukilteo.wednet.org>
2024-10-03 13:36:11 -0700

    bulb

---
+++ Text Adventure/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Text_adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello there! \nWelcome to the world of pokémon! My name is Oak! \nPeople call me the pokémon Prof! \nThis world is inhabited by creatures called pokémon! \nFor some people, pokémon are pets. Others use them for fights. \nMyself...I study pokémon as a profession.");
+            Console.WriteLine("|                                                                                                                                                                                                            |");
+            Console.WriteLine("|                                                  :++-:=*.                                                                                                                                                  |");
+            Console.WriteLine("|                                                :*===*-==:-                                                                                                                                                 |");
+            Console.WriteLine("|                                .:+=---================+++=                                                                                                                                                 |");
+            Console.WriteLine("|                            :+= -----=======+=====-====*+*                                                                                                                                                  |");
+            Console.WriteLine("|                           :*========-=*=----==========*+*                                                                                                                                                  |");
+            Console.WriteLine("|             =:-*=.       .*+========*+================*++-                                                                                                                                                 |");
+            Console.WriteLine("|            = ---==*:::::-----=+*+*=:::--*==========-==++++*.                                                                                                                                               |");
+            Console.WriteLine("|           = ---:::::::::-===-----:-----==+===========+++*+++.                                                                                                                                              |");
+            Console.WriteLine("|           .*=-::::::+++****+-----------==+=========++++++*+++:                                                                                                                                             |");
+            Console.WriteLine("|          = --:::::*++*****--------------=*=++++++++++++++*+++*:                                                                                                                                            |");
+            Console.WriteLine("|         = ----::--==**#=---------------===#+++++++++++++++*===*                                                                                                                                            |");
+            Console.WriteLine("|        = -:+-=-**---------===-=-=*=---====***+++++++++++++*+=++=                                                                                                                                           |");
+            Console.WriteLine("|        -=.+*-+-------+*-----:-=.*+*%=-=======**+++++++++++*+++++                                                                                                                                           |");
+            Console.WriteLine("|        *::*-==-----***+---*--= #+= *#=====+==++%++++++++++++++++                                                                                                                                           |");
+            Console.WriteLine("|       -= =*:=------=#+------:  **+ **#=====+=*****#+++++%++++++*                                                                                                                                           |");
+            Console.WriteLine("|      = - :*=--:-------------:  #**:***========******%#+*+++++++*                                                                                                                                           |");
+            Console.WriteLine("|     = --*+*:-------------==.  =*+++**=========#******+*++++++*.                                                                                                                                            |");
+            Console.WriteLine("|     * +*=----:--------------=-----==+==========#****#+=*+++*#:                                                                                                                                             |");
+            Console.WriteLine("|       :*==#==----=------------===%+=====================**#:                                                                                                                                               |");
+            Console.WriteLine("|          *==%*+**%%%%##*****++**+======*=================*:                                                                                                                                                |");
+            Console.WriteLine("|            :*==*===========+*=======*+=------==+=+==-=====+                                                                                                                                                |");
+            Console.WriteLine("|            .=-=+++=++***+======*+***---------==*+-+***+=====                                                                                                                                               |");
+            Console.WriteLine("|             #---=+===*****++++++++*=--%+*----==#-*++***====#                                                                                                                                               |");
+            Console.WriteLine("|            = -*++*====+*=++++++++*=*++===---====-=+****====%.                                                                                                                                              |");
+            Console.WriteLine("|             += ++*+======+*+*++++%==+#-----===*====**#=====#                                                                                                                                               |");
+            Console.WriteLine("|              .*++========:  .*++*+++*-----==+*%===========+:                                                                                                                                               |"); 
+            Console.WriteLine("|               -========*:     =#*#*#+---===*: .+==========+                                                                                                                                                |");
+            Console.WriteLine("|              - +.+=+*=*         *-----====%     +*=*+=+===                                                                                                                                                 |");
+            Console.WriteLine("|                .   .           +=#::**-+=         .=-:=:                                                                                                                                                   |");
+
+
+
+
+            string input = Console.ReadLine();
+
+            if (input == "asdfghjkl;")
+            {
+
+            }
+
+        }
+    }
+}
+
+
